1ed7da2521d01811f15dd0e35bf729d3510a271d
liushunqi <2294404379@xiaomi.com>
2024-05-16 15:24:00 +0800

    maven

--- helloworld.java
+++ helloworld.java
@@ -5,5 +5,20 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("nihao");
 
+        int[] nums={0,1,2,2,3,0,4,2};
+        System.out.println(nums.length);
+        System.out.println("剩余个数为："+removeElement(nums,2));
+
+    }
+    public static int removeElement(int[] nums, int val) {
+        int count=0;
+        for(int i=0;i<nums.length;++i){
+            if(nums[i]==val){
+                count++;
+            }else {
+                nums[i-count]=nums[i];
+            }
+        }
+        return count;
     }
 }
---
+++ mvn_learn_from_scratch/src/test/java/edu/cn/nuist/DemoTest.java
@@ -0,0 +1,19 @@
+package edu.cn.nuist;
+
+import org.junit.Test;
+
+public class DemoTest {
+    @Test
+    public void test1(){
+        System.out.println("hello test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("hello test 2");
+    }
+    @Test
+    public void test3(){
+        System.out.println("hello test 3");
+    }
+}
